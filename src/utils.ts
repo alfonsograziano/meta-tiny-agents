07669b990c96ac325e9d425214064ab66b68c7cb
alfonsograziano <alfonsohack0019@gmail.com>
2025-08-24 08:43:17 +0200

    new utilities

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,54 @@
+import path from "path";
 import readline from "readline";
+import { promises as fs } from "fs";
+import { fileURLToPath } from "url";
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-export async function askQuestions(questions: string): Promise<string> {
+export async function input(text: string): Promise<string> {
   return new Promise((resolve) => {
-    rl.question("\n" + questions + "\n Answer here:", (input) => {
+    rl.question("\n" + text + "\nAnswer here: ", (input) => {
       resolve(input.trim());
     });
   });
 }
+
+export const getContext = async () => {
+  // Load all the MD files in the context folder
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const contextDir = path.resolve(__dirname, "../context");
+  const files = await fs.readdir(contextDir);
+  const mdFiles = files.filter((file) => file.endsWith(".md"));
+  // Load the content of the MD files
+  const context = await Promise.all(
+    mdFiles.map(async (file) => {
+      const content = await fs.readFile(path.resolve(contextDir, file), "utf8");
+      return {
+        file,
+        content,
+      };
+    })
+  );
+  return context;
+};
+
+export const getContextString = async () => {
+  const context = await getContext();
+  return context.map((item) => `# ${item.file}\n${item.content}`).join("\n");
+};
+
+export const getProfileDir = async () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  return path.resolve(__dirname, "../profiles/.main-profile");
+};
+
+export const getWorkspaceDir = async () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  return path.resolve(__dirname, "../workspace");
+};
