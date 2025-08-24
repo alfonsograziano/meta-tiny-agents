511e00d3609cde575cfd1b50243eb235bcdd5a5c
alfonsograziano <alfonsohack0019@gmail.com>
2025-08-24 08:43:01 +0200

    save results and default MCP servers

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,105 @@
 import { TinyAgent } from "./tinyAgents.js";
 import { OpenAI } from "openai";
-import { fileURLToPath } from "url";
 import path from "path";
-import { askQuestions } from "./utils.ts";
+import {
+  getContextString,
+  getProfileDir,
+  getWorkspaceDir,
+  input,
+} from "./utils.js";
+import fs from "fs";
+
+const goal = await input("How can I help you today?");
+const context = await getContextString();
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY!,
 });
 
 // Create a TinyAgent instance
-const agent = new TinyAgent({});
-
-const goal = "Design a workout routine for a beginner with no equipment.";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const HELLO_SERVER_PATH = path.resolve(
-  __dirname,
-  "..",
-  "test",
-  "fixtures",
-  "helloWorldServer.js"
-);
-await agent
-  .getClientsRegistry()
-  .register("stdio", "hello-world", "node", [HELLO_SERVER_PATH], {
-    PATH: process.env.PATH!,
-  });
-
-const systemPrompt = await agent.generateSystemPrompt({
-  openai,
-  goal,
+const agent = new TinyAgent({
+  maxInteractions: 50,
 });
 
-console.log("Generated System Prompt:\n", systemPrompt);
+await agent
+  .getClientsRegistry()
+  .register(
+    "stdio",
+    "playwright",
+    "npx",
+    ["@playwright/mcp@latest", "--user-data-dir=" + (await getProfileDir())],
+    {
+      PATH: process.env.PATH!,
+    }
+  );
+
+await agent
+  .getClientsRegistry()
+  .register(
+    "stdio",
+    "node-code-interpreter",
+    "npx",
+    ["-y", "node-code-sandbox-mcp"],
+    {
+      PATH: process.env.PATH!,
+      FILES_DIR: await getWorkspaceDir(),
+    }
+  );
+
+await agent
+  .getClientsRegistry()
+  .register(
+    "stdio",
+    "filesystem",
+    "npx",
+    ["-y", "@modelcontextprotocol/server-filesystem", await getWorkspaceDir()],
+    {
+      PATH: process.env.PATH!,
+    }
+  );
+
+// const systemPrompt = await agent.generateSystemPrompt({
+//   openai,
+//   goal: `
+//     Context:
+//     ${context}
+
+//     Goal:
+//     ${goal}
+//     `,
+// });
+const systemPrompt = `You are a helpful assistant`;
 
 const baseMessages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
   { role: "system", content: systemPrompt },
   {
     role: "user",
-    content: goal,
+    content: `
+    Context:
+    ${context}
+
+    Goal:
+    ${goal}
+    `,
   },
 ];
 
 const result = await agent.run({
   openai,
   baseMessages,
-  requestInputFromUser: askQuestions,
+  requestInputFromUser: input,
 });
-console.log("RESULT:", JSON.stringify(result, null, 2));
+
+// Save the result to a /results/new Date.json file
+const resultsDir = path.resolve(
+  path.dirname(new URL(import.meta.url).pathname),
+  "../results"
+);
+const resultsFile = path.resolve(
+  resultsDir,
+  `${new Date().toISOString()}.json`
+);
+fs.writeFileSync(resultsFile, JSON.stringify(result, null, 2));
+await agent.getClientsRegistry().cleanup();
+
 process.exit(0);
